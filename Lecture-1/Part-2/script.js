ff7d14fb64492c76914f9ba9582c590383f80edd
ketanmakwana30 <ketanmakwana0130@gmail.com>
2025-12-14 01:46:25 +0530

    if else statment

--- Lecture-1/Part-2/script.js
+++ Lecture-1/Part-2/script.js
@@ -1,2 +1,47 @@
 //if statement
 
+// let age = 19;
+// if (age >= 18) {
+//     console.log("You can Vote");   
+// }
+
+// let firstName = "Ketan";
+// if (firstName == "Ketan") {
+//     console.log(`Welcome ${firstName}`);
+    
+// }
+
+
+//Traffic Light System
+
+// let color = "green";
+
+// if (color == "red") {
+//     console.log("Stop");
+// }
+// if (color == "green"){
+//     console.log("Go");
+// }
+// if (color == "yellow") {
+//     console.log("Slow Down");
+    
+// }
+
+
+//else if statement
+
+let marks = 23;
+if (marks >= 90) {
+    console.log("Gread: A");
+}else if (marks >= 80) {
+    console.log("Gread: B");
+}else if (marks >= 70) {
+    console.log("Gread: C");
+}else if (marks >= 50) {
+    console.log("Gread: D");
+}else if (marks >= 33) {
+    console.log("Gread: E");
+}else{
+    console.log("You age Fail");
+    
+}
