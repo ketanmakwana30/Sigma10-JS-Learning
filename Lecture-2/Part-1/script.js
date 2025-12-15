540bec853158275140612e16a144d6dbf4c28370
ketanmakwana30 <ketanmakwana0130@gmail.com>
2025-12-15 21:47:41 +0530

    add trim method

---
+++ Lecture-2/Part-1/script.js
@@ -0,0 +1,10 @@
+//trim method
+
+// let str = "    Hello     world!    ";
+// str.trim();
+
+
+// let password = prompt("Enter your password");
+// let newPassword = password.trim();              //use trim() method
+// console.log(password);
+
