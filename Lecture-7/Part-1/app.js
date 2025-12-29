eb4ea6953ab9b4d3932a350288cd9d4655490933
ketanmakwana30 <ketanmakwana0130@gmail.com>
2025-12-29 22:33:32 +0530

    Arrow function added

---
+++ Lecture-7/Part-1/app.js
@@ -0,0 +1,35 @@
+//  this as calling object
+
+const student = {
+    name: "ketan",
+    age: 19,
+    math: 91,
+    eco: 89,
+    guj: 79,
+    getAvg(){
+        let avg = (this.math + this.eco + this.guj) / 3;
+        console.log(avg);
+        
+    }
+}
+
+
+// Try & catch statement
+
+console.log("hello!");
+// let a = 10;
+try{
+    console.log(a);
+} catch(err) {
+    console.log("Error: a is not defined");
+    console.log(err);
+}
+console.log("hello!");
+
+
+// Arrow function
+
+const sum = (a, b) => {
+    return a+b;
+};
+console.log(sum(5,7));
