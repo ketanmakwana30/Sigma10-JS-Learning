b4eb2c8b5bc68137e0f000046b11a1ef1790e38d
ketanmakwana30 <ketanmakwana0130@gmail.com>
2026-02-21 22:24:18 +0530

    promise chaining

--- Lecture-11/Part-3/app.js
+++ Lecture-11/Part-3/app.js
@@ -1,12 +1,30 @@
 // Promises
 
-function savetoDB(data){
-    return new Promise((resolve, reject) =>{
-        let internetSpeed = Math.floor(Math.random() * 10) + 1;
-        if (internetSpeed > 4){
-            resolve("Complete");
+function getData (data) {
+    return new Promise ( (resolve, reject) => {
+        let internetSpeed = Math.floor(Math.random() * 10 ) + 1;
+        if  ( internetSpeed > 4 ){
+            resolve("Your Data is Saved.");
         } else {
-            reject("Error");  
+            reject("Error : 400");
         }
-    })
+    });  
 };
+
+// Promise Chaining
+
+getData("Apna College")
+    .then( () => {
+        console.log("Data 1 success ");
+        return getData("Shradha Khapra");
+    })
+    .then( () => {
+        console.log("Data 2 success ");
+        return getData("Ketan Makwana");
+    })
+    .then( () => {
+        console.log("Data 3 success ");
+    })
+    .catch( () => {
+        console.log(" promise was Rejected");
+    });
